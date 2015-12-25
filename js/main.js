6229b8cbcacca3cdade4f01b5d5cb7116de957a0
Hitesh Goel <hitukkp@gmail.com>
2015-12-26 00:17:41 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-var htmldata = '<div class="events"><div class="ride_name"></div><div class="ride_picture"></div><div class="ride_date"></div></div><div class="events"><div class="ride_name"></div><div class="ride_picture"></div><div class="ride_date"></div></div><div class="events"><div class="ride_name"></div><div class="ride_picture"></div><div class="ride_date"></div></div>';
-var ni = document.getElementById('body_content');
-ni.innerHTML += htmldata;
-
 $.ajax({
 	url: '/json/event_data.json',
 	type: 'GET',
 	success: function(data){
 		console.log(data.data);
+		htmldata = create_event_html(data.data);
+		var ni = document.getElementById('body_content');
+		ni.innerHTML += htmldata;
 	}
 });
+
+function create_event_html( raw_data ){
+	var tempHtml = '';
+	for(var i=0 ; i<raw_data.length; i++){
+		console.log(raw_data[i]);
+		tempHtml += '<div class="events"><div class="ride_name">'+raw_data[i].event_title+'</div>';
+		tempHtml +=  '<div class="ride_picture"><img src="'+raw_data[i].event_image+'"></div>';
+		tempHtml +=  '<div class="ride_date">'+raw_data[i].event_date_duration+'</div></div>'
+	}
+	var htmldata = tempHtml;
+	return htmldata;
+}
