906b034d20d4a08b71733fa7abcd8b67efb824ec
Hitesh Goel <hitukkp@gmail.com>
2015-12-26 14:06:03 +0530

    sjahgds

--- js/event_details_main.js
+++ js/event_details_main.js
@@ -5,9 +5,23 @@ function getParameterByName(name) {
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 var event_id = getParameterByName('event_id');
-var listvalues = localStorage.getItem('event_'+event_id);
-var finalvalue = JSON.parse(listvalues);
-console.log(finalvalue);
+var event_values = localStorage.getItem('event_'+event_id);
+var event_details = JSON.parse(event_values);
+console.log(event_details);
 
-var ni = document.getElementById('body_content');
-ni.innerHTML += htmldata;
+var event_content = document.getElementById('event_content');
+event_content.innerHTML = event_details.event_html;
+
+var total_distance = document.getElementById('total_distance');
+total_distance.innerHTML = event_details.total_kms;
+
+var date_duration = document.getElementById('date_duration');
+date_duration.innerHTML = event_details.event_date_duration;
+
+var time_duration = document.getElementById('time_duration');
+time_duration.innerHTML = event_details.event_time_duration;
+
+var event_title = document.getElementById('event_title_h2');
+event_title.innerHTML = event_details.event_name;
+
+document.getElementById("event_main_cover").src=event_details.event_image;
